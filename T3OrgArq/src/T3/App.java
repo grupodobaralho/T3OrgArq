cff4dd61f79baad51f831ec71d1bc6877c1022cc
Israel Deorce <israel.idvj@gmail.com>
2017-06-02 07:04:02 -0300

    Primeira pesquisa implementada

--- T3OrgArq/src/T3/App.java
+++ T3OrgArq/src/T3/App.java
@@ -10,11 +10,36 @@ public class App {
 
 		String[] teste = converser.read();
 
+		/*
 		for (int i = 0; i < teste.length; i++) {
 			if (teste[i] != null)
 				System.out.println(teste[i]);
 
 		}
-
+		*/
+		
+		//ESTE FOI UM TESTE DE MESA QUE EU FIZ E FUNCIONOU!
+		/*
+		Cache cache = new Cache(4,8);
+		System.out.printf("%-9s %-9s %-9s %-9s %-9s %-9s %-9s %-9s %-9s\n", "TAG", "000", "001", "010", "011", "100", "101", "110", "111");
+		System.out.println("-----------------------------------------------------------------------------------------");
+		cache.add("00101011");
+		cache.add("00101011");
+		cache.add("00101010");
+		cache.add("11011111");
+		cache.printa();
+		*/
+		
+		//TESTANDO COM A LEITURA DO VINIBOY
+		System.out.printf("%-9s %-9s %-9s %-9s %-9s %-9s %-9s %-9s %-9s\n", "TAG", "000", "001", "010", "011", "100", "101", "110", "111");
+		System.out.println("-----------------------------------------------------------------------------------------");
+		Cache cache = new Cache(4,8);
+		for(int i=0; i<teste.length; i++) {
+			cache.add(teste[i]);
+		}
+		cache.printa();
+		
+		
+		
 	}
 }
--- T3OrgArq/src/T3/Cache.java
+++ T3OrgArq/src/T3/Cache.java
@@ -3,28 +3,107 @@ package T3;
 import java.util.ArrayList;
 
 public class Cache {
-		
-	String[][] cache;	
-	int tLinha;
-	int tColuna;
-	
-	ArrayList<String> relatorio;
-	
-	
-	public Cache(int qtLinhas, int qtBlocos){
-		
-		//Quantidade de linhas da Cache
+
+	private String[][] cache;
+	private int tLinha;
+	private int tColuna;
+
+	private ArrayList<String> relatorio;
+
+	public Cache(int qtLinhas, int qtBlocos) {
+
+		// Quantidade de linhas da Cache
 		this.tLinha = qtLinhas;
-		//Quantidade de blocos (colunas) + a tag
+		// Quantidade de blocos (colunas) + a tag
 		this.tColuna = qtBlocos + 1;
 		cache = new String[tLinha][tColuna];
-		
+
 		relatorio = new ArrayList<>();
-		
+
 	}
-	
-	
-	
-	
+
+	public void add(String endereco) {
+
+		if (tLinha == 4 && tColuna == 9) {
+			adicionaUm(endereco);
+		} else if (tLinha == 8 && tColuna == 5) {
+			adicionaDois(endereco);
+		} else if (tLinha == 16 && tColuna == 3) {
+			adicionaTres(endereco);
+		} else {
+			throw new RuntimeException("Erro no add da classe Cache!");
+		}
+
+	}
+
+	// Mapeamento direto, com 3 bits para tag, 2 bits para linha e 3 bits
+	// para palavra (cache com 4 linhas, 8 palavras por linha).
+	public void adicionaUm(String endereco) {
+		String tag = endereco.substring(0, 3);
+		int linha = Integer.parseInt(endereco.substring(3, 5), 2);
+		int palavra = Integer.parseInt(endereco.substring(5, 8), 2) + 1;
+
+		if (cache[linha][0] == null || !cache[linha][0].equals(tag) || !cache[linha][palavra].equals(endereco)) {
+			relatorio.add("Miss;");
+			cache[linha][0] = tag;
+			for (int i = 1; i < tColuna; i++) {
+				switch (i) {
+				case 1:
+					cache[linha][i] = endereco.substring(0, 5) + "000";	break;
+				case 2:
+					cache[linha][i] = endereco.substring(0, 5) + "001";	break;
+				case 3:
+					cache[linha][i] = endereco.substring(0, 5) + "010";	break;
+				case 4:
+					cache[linha][i] = endereco.substring(0, 5) + "011";	break;
+				case 5:
+					cache[linha][i] = endereco.substring(0, 5) + "100";	break;
+				case 6:
+					cache[linha][i] = endereco.substring(0, 5) + "101";	break;
+				case 7:
+					cache[linha][i] = endereco.substring(0, 5) + "110";	break;
+				case 8:
+					cache[linha][i] = endereco.substring(0, 5) + "111";	break;
+				default:
+					throw new RuntimeException("Erro no switch case");
+				}
+			}
+		} else {
+			relatorio.add("HIT;");
+		}
+	}
+
+	private void adicionaDois(String endereco) {
+		//FALTA FAZER (SEGUIR MODELO DO DE CIMA)
+	}
+
+	private void adicionaTres(String endereco) {
+		//FALTA FAZER (SEGUIR MODELO DO DE CIMA)
+	}
+
+	public void printa() {
+		for (int i = 0; i < tLinha; i++) {
+			for (int j = 0; j < tColuna; j++) {
+				if (cache[i][j] == null) {
+					System.out.printf("%-10s", "NULL ");
+				} else {
+					System.out.printf("%-10s", cache[i][j] + " ");
+				}
+
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+		int count = 0;
+		for (String str : relatorio) {
+			if (count == 15) {
+				System.out.println();
+				count = 0;
+			}
+			System.out.printf(str);
+			count++;
+		}
+	}
 
 }
