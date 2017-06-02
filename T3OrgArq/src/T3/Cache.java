b6fc26e7145ca936bade9c57ccca26af751d8902
Israel Deorce <israel.idvj@gmail.com>
2017-06-01 22:40:49 -0300

    Cache criada

--- T3OrgArq/src/T3/Cache.java
+++ T3OrgArq/src/T3/Cache.java
@@ -1,5 +1,30 @@
 package T3;
 
+import java.util.ArrayList;
+
 public class Cache {
+		
+	String[][] cache;	
+	int tLinha;
+	int tColuna;
+	
+	ArrayList<String> relatorio;
+	
+	
+	public Cache(int qtLinhas, int qtBlocos){
+		
+		//Quantidade de linhas da Cache
+		this.tLinha = qtLinhas;
+		//Quantidade de blocos (colunas) + a tag
+		this.tColuna = qtBlocos + 1;
+		cache = new String[tLinha][tColuna];
+		
+		relatorio = new ArrayList<>();
+		
+	}
+	
+	
+	
+	
 
 }
