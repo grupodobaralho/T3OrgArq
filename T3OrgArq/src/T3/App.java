c686db4431f49443118a8d3d2973c8fc2efd93c7
ViniKroth <vinicius.kroth@acad.pucrs.br>
2017-06-01 22:43:12 -0300

    Implementar leitura, implementar hexToBin

--- T3OrgArq/src/T3/App.java
+++ T3OrgArq/src/T3/App.java
@@ -1,16 +1,20 @@
 package T3;
 
+import java.io.IOException;
+
 public class App {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 
-	HexToBin converser = new HexToBin();
-	
-	
-	String teste = converser.hexToBin("0B");
-	
-	System.out.println(teste);
-	
+		HexToBin converser = new HexToBin();
+
+		String[] teste = converser.read();
+
+		for (int i = 0; i < teste.length; i++) {
+			if (teste[i] != null)
+				System.out.println(teste[i]);
+
+		}
+
 	}
-
 }
--- T3OrgArq/src/T3/HexToBin.java
+++ T3OrgArq/src/T3/HexToBin.java
@@ -1,27 +1,47 @@
 package T3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HexToBin {
 
-	
-	StringBuffer str = new StringBuffer();
-	
-	public String hexToBin(String hex) {
+	public static String[] read() throws IOException {
 		String bin = "";
 		String binFragment = "";
-		int iHex;
-		hex = hex.trim();
+		String[] retorno = new String[407];
+		try {
+			int cont = 0;
+			BufferedReader br = new BufferedReader(new FileReader("Files/T3input.txt"));
+			String number = "";
+			while (br.ready()) {
+				number = br.readLine();
+				bin = "";
+				binFragment = "";
+				int iHex;
+				number = number.trim();
 
-		for (int i = 0; i < hex.length(); i++) {
-			iHex = Integer.parseInt("" + hex.charAt(i), 16);
-			binFragment = Integer.toBinaryString(iHex);
+				for (int i = 0; i < number.length(); i++) {
+					iHex = Integer.parseInt("" + number.charAt(i), 16);
+					binFragment = Integer.toBinaryString(iHex);
 
-			while (binFragment.length() < 4) {
-				binFragment = "0" + binFragment;
+					while (binFragment.length() < 4) {
+						binFragment = "0" + binFragment;
+					}
+					bin += binFragment;
+					retorno[cont] = bin;
+				}
+
+				cont++;
 			}
-			bin += binFragment;
-		}
-		return bin;
 
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+
+		return retorno;
 	}
 
 }
